641a3b98215a06519b3c5098fdb088af15c09068
Marcela Griet <marcelagriet@gmail.com>
2021-07-22 10:18:49 -0300

    working ok with axios

--- src/components/Forms/PostForm.jsx
+++ src/components/Forms/PostForm.jsx
@@ -17,21 +17,8 @@ const MyPostForm = () => {
 
     const savePost = async (newPost) => {
         try{
-           /*  await axios.post('https://jsonplaceholder.typicode.com/posts', newPost);
-            console.log(" info sent to post endopint:", newPost) */
-            fetch('https://jsonplaceholder.typicode.com/posts', {
-            method: 'POST',
-            body: JSON.stringify({
-                title: newPost.title,
-                body: newPost.content
-            }),
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            },
-            }) 
-            .then((response) => response.json())
-            .then((json) => console.log(json));
-            
+            const resp = await axios.post('https://jsonplaceholder.typicode.com/posts', newPost);
+            console.log(" info sent to post endopint:", resp.data)
         }
         catch (error) {
             message.error("Failed to create post. Clear your entries and check fields requirements shown in red")
